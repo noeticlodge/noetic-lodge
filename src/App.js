ea1689789240e1ce52fda0c25403549d2290b72c
Karen Andres <karenlandres@gmail.com>
2021-07-29 11:18:37 -0400

    logo update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Noetic Lodge</h1>
         <h3>where healing happens</h3>
       </header>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+        
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+        
+        <input type="submit" />
+      </form>
+
     </div>
   );
 }
